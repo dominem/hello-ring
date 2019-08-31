e373b6b242961130beed860a59cb48a91bc46872
Dominik Magdaleński <dominik.magdalenski@gmail.com>
2019-08-31 18:57:25 +0200

    Add about page and simple routing

--- src/hello_ring/core.clj
+++ src/hello_ring/core.clj
@@ -6,36 +6,71 @@
 
 (def bootstrap-css
   [:link
-   {:rel "stylesheet"
-    :href "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    :integrity "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+   {:rel         "stylesheet"
+    :href        "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    :integrity   "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
     :crossorigin "anonymous"}])
 
 (def head
   [:head
    [:meta {:charset "utf-8"}]
-   [:meta {:name "viewport"
+   [:meta {:name    "viewport"
            :content "width=device-width, initial-scale=1, shrink-to-fit=no"}]
    bootstrap-css
    [:title "Hello, Ring!"]])
 
-(defn body [request]
-  [:body
-   [:header
-    [:nav.navbar.navbar-expand-lg.navbar-light.bg-light
-     [:div.container.justify-content-center
-      [:a.navbar-brand.m-0 {:href "/"} "Hello, Ring!"]]]]
-   [:main.mt-4
-    [:div.container
-     [:h1.mb-4 "Your request map"]
-     (pretty/map->hiccup request)]]])
+(defn body-header-nav-item [uri href text]
+  [(if (= uri href)
+     :li.nav-item.active
+     :li.nav-item)
+   [:a.nav-link {:href href} text]])
 
-(defn handler [request]
+(defn body-header-nav [uri]
+  [:ul.navbar-nav
+   (body-header-nav-item uri "/" "Home")
+   (body-header-nav-item uri "/about" "About")])
+
+(defn body-header [request]
+  [:header
+   [:nav.navbar.navbar-expand-lg.navbar-light.bg-light
+    [:div.container.justify-content-between
+     [:a.navbar-brand.m-0 {:href "/"} "Hello, Ring!"]
+     (body-header-nav (:uri request))]]])
+
+(defn body-main [content]
+  [:main.mt-4 [:div.container content]])
+
+(defn body [request & content]
+  [:body
+   (body-header request)
+   (body-main content)])
+
+(defn home-page [request]
   {:status  200
    :headers {"Content-Type" "text/html"}
    :body    (hiccup/html
               (hiccup-page/doctype :html5)
-              [:html {:lang "en"} head (body request)])})
+              [:html {:lang "en"} head
+               (body request
+                     [:h1.mb-4 "Your request map"]
+                     (pretty/map->hiccup request))])})
+
+(defn about-page [request]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    (hiccup/html
+              (hiccup-page/doctype :html5)
+              [:html {:lang "en"} head
+               (body request
+                     [:h1.mb-4 "About"]
+                     [:p.lead (str "This is just my ring sandbox. "
+                                   "I learn ring here.")])])})
+
+(defn route [request]
+  (case (:uri request)
+    "/" (home-page request)
+    "/about" (about-page request)
+    {:status 404}))
 
 (defn -main [request]
-  (handler request))
+  (route request))
